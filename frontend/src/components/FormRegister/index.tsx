e1f7eb94bf068eecd304a48cc5603f61ea91bae6
Priscila Ribeiro <priscilt.ribeiro@hotmail.com>
2021-03-23 10:20:02 -0300

    Validators input

--- frontend/src/components/FormRegister/index.tsx
+++ frontend/src/components/FormRegister/index.tsx
@@ -13,7 +13,6 @@ interface FormProps {
 }
 
 function cpf(value:string) {
-  console.log(value)
   value = value.replace(/\D/g,"") 
 
   if(value.length >11)
@@ -22,8 +21,6 @@ function cpf(value:string) {
       value = value.replace(/(\d{3})(\d)/, '$1.$2') 
       value = value.replace(/(\d{3})(\d)/, '$1.$2') 
       value = value.replace(/(\d{3})(\d)/, '$1-$2') 
-
-  console.log(value)
 
   return value
 
@@ -41,7 +38,33 @@ function cnpj(value:string) {
       value = value.replace(/(\d{4})(\d)/, '$1-$2') 
 
   return value
+}
 
+function email(value:string) {
+  let error = false
+  const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  
+  if(!value.match(mailFormat))
+    error = true
+
+  return error
+}
+
+function phone(value:string) {
+  value = value.replace(/\D/g,"") 
+
+  if(value.length >11)
+    value = value.slice(0, -1)
+
+    if (value.length >10) {
+      value = value.replace(/(\d{2})(\d)/, '($1) $2') 
+      value = value.replace(/(\d{5})(\d)/, '$1-$2')
+    } else{
+      value = value.replace(/(\d{2})(\d)/, '($1) $2') 
+      value = value.replace(/(\d{4})(\d)/, '$1-$2')
+    }
+
+    return value
 }
 
 export function FormRegister() {
@@ -54,7 +77,15 @@ export function FormRegister() {
     address: ''
   })
 
-  console.log(form)
+  const [error, setError] = useState({
+    name: false,
+    cpf: false,
+    cnpj: false,
+    email: false,
+    phone: false,
+    address: false
+  })
+
 
   return (
    <Container>
@@ -65,7 +96,19 @@ export function FormRegister() {
             ...form,
             name: event.target.value
           })}
+          value={form.name}
+
+          onBlur={event => {
+            setError({
+              ...error,
+              name: (form.name == "")
+            })
+          }}
+          className={error.name ? 'error': ''}
         />
+
+        { error.name && <span>Coloque seu nome completo :)</span> }
+
 
         <input 
           placeholder="CPF"
@@ -74,7 +117,18 @@ export function FormRegister() {
             cpf: cpf(event.target.value)
           })}
           value={form.cpf}
+
+          onBlur={event => {
+            setError({
+              ...error,
+              cpf: (form.cpf == "" || form.cpf.length < 10) 
+            })
+          }}
+          className={error.cpf ? 'error': ''}
         />
+
+        { error.cpf && <span>Por favor, informe seu CPF</span> }
+      
 
         <input 
           placeholder="CNPJ MEI"
@@ -83,23 +137,59 @@ export function FormRegister() {
             cnpj: cnpj(event.target.value)
           })}
           value={form.cnpj}
+
+          onBlur={event => {
+            setError({
+              ...error,
+              cnpj: (form.cnpj == "" || form.cnpj.length < 13) 
+            })
+          }}
+          className={error.cnpj ? 'error': ''}
         />
+
+        { error.cnpj && <span>Por favor, informe seu CNPJ MEI</span> }
+        
+
 
         <input 
           placeholder="Email"
-          onChange={event => setForm({
-            ...form,
-            email: event.target.value
-          })}
+          onChange={event => {
+            setForm({
+              ...form,
+              email: event.target.value
+            })}
+          }
+          onBlur={event => {
+            setError({
+              ...error,
+              email: email(event.target.value)
+            })
+          }}
+          value={form.email}
+          className={error.email ? 'error': ''}
         />
+
+        {error.email && <span>Email inválido, tente novamente</span> }
 
         <input 
           placeholder="Telefone"
           onChange={event => setForm({
             ...form,
-            phone: event.target.value
+            phone: phone(event.target.value)
           })}
+          value={form.phone}
+
+          onBlur={event => {
+            setError({
+              ...error,
+              phone: (form.phone == "" || form.phone.length < 9) 
+            })
+          }}
+          className={error.phone ? 'error': ''}
         />
+
+        { error.phone && <span>Por favor, informe seu telefone</span> }
+        
 
         <input 
           placeholder="Endereço"
@@ -107,7 +197,17 @@ export function FormRegister() {
             ...form,
             address: event.target.value
           })}
+          onBlur={event => {
+            setError({
+              ...error,
+              address: (form.address == "")
+            })
+          }}
+          className={error.address ? 'error': ''}
         />
+
+        { error.address && <span>Por favor, coloque seu endereço completo</span> }
+
 
         <SelectSelfie />
 
